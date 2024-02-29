b8984b8b1daa341c262d775f9e00802ef454ff62
yisus sensei <jesus.andres1996@hotmail.com>
2024-02-29 11:38:14 -0500

    creación primer achivo de migracion

---
+++ app/Database/Migrations/2024-02-29-163551_CreateTableStatesUsers.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateTableStatesUsers extends Migration
+{
+    public function up()
+    {
+        //
+        $this->forge->addField([
+            ''
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
