b0b9c44186eeb172be82972fd6e89cf121775420
Daniel Avendaño <dalaven1996@gmail.com>
2024-12-06 08:02:30 -0500

    implement env variables

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,9 +27,9 @@ class Database extends Config
     public array $default = [
         'DSN'          => '',
         'hostname'     => 'localhost',
-        'username'     => 'root',
-        'password'     => 'Dalaven123!!!',
-        'database'     => 'amas',
+        'username'     => '',
+        'password'     => '',
+        'database'     => '',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
