bce482d45be103004d279663b9d64df0a4560ac5
Daniel Avendaño <dalaven1996@gmail.com>
2024-12-06 10:55:10 -0500

    minor fix

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://172.20.14.182:80/';
+    public string $baseURL = '';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
