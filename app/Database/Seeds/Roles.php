046638545be0d8fa6976157047b6eab6f8904348
Daniel Avendaño <dalaven1996@gmail.com>
2024-12-09 09:47:56 -0500

    seeders updated

--- app/Database/Seeds/Roles.php
+++ app/Database/Seeds/Roles.php
@@ -8,38 +8,42 @@ class Roles extends Seeder
 {
     public function run()
     {
-        // cargamos datos base de la tabla para roles
+        // Cargamos datos base de la tabla para roles
         $dataRoles = [
-            ['ROLE_name' => 'SUPERADMIN', 'ROLE_description' => 'super administrador', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1],
-            ['ROLE_name' => 'USUARIOS', 'ROLE_description' => 'registro de usuarios', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1],
-            ['ROLE_name' => 'CRUZES', 'ROLE_description' => 'registro de cruzes', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1],
-            ['ROLE_name' => 'ADMIN', 'ROLE_description' => 'registro casos herramientas', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1],
-            ['ROLE_name' => 'VALORACION 1448', 'ROLE_description' => 'Ajustes para 1448', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1],
+            ['ROLE_name' => 'SUPERADMIN', 'ROLE_description' => 'super administrador', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1, "ROLE_state" => 1],
+            ['ROLE_name' => 'USUARIOS', 'ROLE_description' => 'registro de usuarios', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1, "ROLE_state" => 1],
+            ['ROLE_name' => 'CRUZES', 'ROLE_description' => 'registro de cruzes', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1, "ROLE_state" => 1],
+            ['ROLE_name' => 'ADMIN', 'ROLE_description' => 'registro casos herramientas', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1, "ROLE_state" => 1],
+            ['ROLE_name' => 'VALORACION 1448', 'ROLE_description' => 'Ajustes para 1448', 'ROLE_date_create' => date('Y-m-d H:i:s'), 'ROLE_date_update' => date('Y-m-d H:i:s'), 'ROLE_FK_user_create' => 1, 'ROLE_FK_user_update' => 1, "ROLE_state" => 1],
 
         ];
         $this->db->table('roles')->insertBatch($dataRoles);
 
-        // cargamos datos base de la tabla para permisos
+        // Cargamos datos base de la tabla para permisos
         $dataPermissions = [
-            ['PRMS_name' => 'consulta usuario', 'PRMS_description' => 'consulta usuario', 'PRMS_system_name' => 'C_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'editar usuarios', 'PRMS_description' => 'editar usuarios', 'PRMS_system_name' => 'E_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'inactivar usuarios', 'PRMS_description' => 'inactivar usuarios', 'PRMS_system_name' => 'I_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'crear usuarios', 'PRMS_description' => 'crear usuarios', 'PRMS_system_name' => 'CR_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'consulta roles', 'PRMS_description' => 'consulta roles', 'PRMS_system_name' => 'C_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'editar roles', 'PRMS_description' => 'editar roles', 'PRMS_system_name' => 'E_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'inactivar roles', 'PRMS_description' => 'inactivar roles', 'PRMS_system_name' => 'I_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'crear roles', 'PRMS_description' => 'crear roles', 'PRMS_system_name' => 'CR_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'asignar roles', 'PRMS_description' => 'asignar roles', 'PRMS_system_name' => 'A_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'consulta permisos', 'PRMS_description' => 'consulta permisos', 'PRMS_system_name' => 'C_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'editar permisos', 'PRMS_description' => 'editar permisos', 'PRMS_system_name' => 'E_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'inactivar permisos', 'PRMS_description' => 'inactivar permisos', 'PRMS_system_name' => 'I_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'crear permisos', 'PRMS_description' => 'crear permisos', 'PRMS_system_name' => 'CR_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
-            ['PRMS_name' => 'asignar permisos', 'PRMS_description' => 'asignar permisos', 'PRMS_system_name' => 'A_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1],
+            ['PRMS_name' => 'Consulta usuario', 'PRMS_description' => 'Consulta usuario', 'PRMS_system_name' => 'C_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Editar usuarios', 'PRMS_description' => 'Editar usuarios', 'PRMS_system_name' => 'E_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Inactivar usuarios', 'PRMS_description' => 'Inactivar usuarios', 'PRMS_system_name' => 'I_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Crear usuarios', 'PRMS_description' => 'Crear usuarios', 'PRMS_system_name' => 'CR_USERS', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Consulta roles', 'PRMS_description' => 'Consulta roles', 'PRMS_system_name' => 'C_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Editar roles', 'PRMS_description' => 'Editar roles', 'PRMS_system_name' => 'E_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Inactivar roles', 'PRMS_description' => 'Inactivar roles', 'PRMS_system_name' => 'I_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Crear roles', 'PRMS_description' => 'Crear roles', 'PRMS_system_name' => 'CR_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Asignar roles', 'PRMS_description' => 'Asignar roles', 'PRMS_system_name' => 'A_ROLES', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Consulta permisos', 'PRMS_description' => 'Consulta permisos', 'PRMS_system_name' => 'C_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Editar permisos', 'PRMS_description' => 'Editar permisos', 'PRMS_system_name' => 'E_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Inactivar permisos', 'PRMS_description' => 'Inactivar permisos', 'PRMS_system_name' => 'I_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Crear permisos', 'PRMS_description' => 'Crear permisos', 'PRMS_system_name' => 'CR_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Asignar permisos', 'PRMS_description' => 'Asignar permisos', 'PRMS_system_name' => 'A_PERMI', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'VERIFICACION_CRUCES', 'PRMS_description' => 'Descripción cruces realizados', 'PRMS_system_name' => 'VER_CRUCE', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Consultar usuarios apps', 'PRMS_description' => 'Permite consultar usuarios en varias aplicaciones', 'PRMS_system_name' => 'C_USERS_APP', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Registro poblacionales', 'PRMS_description' => 'Modulo de control para registros poblacionales', 'PRMS_system_name' => 'REG_POBLA', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
+            ['PRMS_name' => 'Migrar censo registro poblacional', 'PRMS_description' => 'Permiso para migrar censos', 'PRMS_system_name' => 'M_CEN_REG_POBLA', 'PRMS_date_create' => date('Y-m-d H:i:s'), 'PRMS_date_update' => date('Y-m-d H:i:s'), 'PRMS_user_create' => 1, 'PRMS_user_update' => 1,"PRMS_state" => 1],
 
         ];
         $this->db->table('permissions')->insertBatch($dataPermissions);
 
-        // cargamos datos base de la tabla para permisos que pertenecen al rol 1
+        // Cargamos datos base de la tabla para permisos que pertenecen al rol 1
         $dataRolesPermissions = [
             ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 1, 'RLPR_FK_rol' => 1],
             ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 2, 'RLPR_FK_rol' => 1],
@@ -55,17 +59,21 @@ class Roles extends Seeder
             ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 12, 'RLPR_FK_rol' => 1],
             ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 13, 'RLPR_FK_rol' => 1],
             ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 14, 'RLPR_FK_rol' => 1],
+            ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 15, 'RLPR_FK_rol' => 1],
+            ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 16, 'RLPR_FK_rol' => 1],
+            ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 17, 'RLPR_FK_rol' => 1],
+            ['RLPR_date_create' => date('Y-m-d H:i:s'), 'RLPR_date_update' => date('Y-m-d H:i:s'), 'RLPR_user_create' => 1, 'RLPR_user_update' => 1, 'RLPR_FK_permission' => 18, 'RLPR_FK_rol' => 1],
         ];
         $this->db->table('rolespermissions')->insertBatch($dataRolesPermissions);
 
-        // cargamos datos base de la tabla de usuario
+        // Cargamos datos base de la tabla de usuario
         $dataUsers = [
-            [ 'USER_name'=>'admin','USER_username' => 'administrador','USER_password' => 'admin123' , 'USER_date_create' => date('Y-m-d H:i:s'),'USER_date_update' => date('Y-m-d H:i:s'),'USER_FK_state_user' => 1],
+            [ 'USER_name'=>'admin','USER_username' => 'administrador','USER_password' => password_hash('admin123', PASSWORD_DEFAULT), 'USER_date_create' => date('Y-m-d H:i:s'),'USER_date_update' => date('Y-m-d H:i:s'),'USER_FK_state_user' => 1],
 
         ];
         $this->db->table('users')->insertBatch($dataUsers);
 
-        // cargamos datos base de la tabla de roles para le usuario
+        // Cargamos datos base de la tabla de roles para le usuario
         $dataUsersRoles = [
             [ 'USRL_FK_user'=> 1,'USRL_FK_rol' => 1,'USRL_date_create' => date('Y-m-d H:i:s') , 'USRL_date_update' => date('Y-m-d H:i:s'),'USRL_user_create' => 1,'USRL_user_update' => 1],
 
