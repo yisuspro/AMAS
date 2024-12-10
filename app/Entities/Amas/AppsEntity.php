c1a3a8f799bc5ac4ab1192b5d49ce18efdf6e584
yisus sensei <jesus.andres1996@hotmail.com>
2024-12-10 15:25:57 -0500

    Create entitys amas

---
+++ app/Entities/Amas/AppsEntity.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Entities\Amas;
+
+use CodeIgniter\Entity\Entity;
+
+class AppsEntity extends Entity
+{
+    protected $datamap = [];
+    protected $dates   = ['created_at', 'updated_at', 'deleted_at'];
+    protected $casts   = [];
+}
