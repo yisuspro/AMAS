861e24c56ccb645e16901f03fd5e6719c8c5fb93
yisus sensei <jesus.andres1996@hotmail.com>
2024-12-13 09:15:14 -0500

    configuración modelos faltantes

--- app/Controllers/Amas/AuditoryController.php
+++ app/Controllers/Amas/AuditoryController.php
@@ -20,7 +20,7 @@ class AuditoryController extends BaseController
         return view('private/views_ajax/Amas/listMyCaseAjax', ['title' => 'Mis casos']);
     }
 
-    public function listMyCase()
+    public function listMyCaseView()
     {
         //
 
--- app/Models/Amas/CasesModel.php
+++ app/Models/Amas/CasesModel.php
@@ -9,9 +9,23 @@ class CasesModel extends Model
 {
     protected $table            = 'cases';
     protected $primaryKey       = 'CASE_PK';
-    protected $useAutoIncrement = true;
+    
     protected $returnType       = CasesEntity::class;
     protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = [
+        'CASE_number',
+        'CASE_date_reception',
+        'CASE_date_solution',
+        'CASE_FK_agent',
+        'CASE_FK_tipe_case',
+        'CASE_FK_state_case',
+        'CASE_FK_entities',
+        'CASE_FK_dependence',
+        'CASE_FK_case_categorie',
+        'CASE_FK_app' 
+    ];
+
+
+   
 }
