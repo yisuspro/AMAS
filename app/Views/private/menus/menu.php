52efde42c8f77a14b04390af1f2b7774f7b3f787
Daniel Avendaño <dalaven1996@gmail.com>
2024-12-17 12:08:05 -0500

    first form

--- app/Views/private/menus/menu.php
+++ app/Views/private/menus/menu.php
@@ -6,7 +6,8 @@
     </div>
     <div class="botones-menu-sup">      
          <!-- Otros elementos del menú -->
-         <a class="logout-boton" id="cerrarSesion"><i class="bi bi-box-arrow-right"></i></a>
+        <p>Hola, <?= session()->get('USER_name') ?></p>
+        <a class="logout-boton" id="cerrarSesion"><i class="bi bi-box-arrow-right"></i></a>
     </div>
 </nav>
 <div class="line-header">
--- app/Views/private/views_ajax/persons/adminPersons.php
+++ app/Views/private/views_ajax/persons/adminPersons.php
@@ -1,64 +1,113 @@
-<div class="card">
-    <div class="titulo-uno">
-        <i class="bi bi-file-earmark-person">Crear persona</i>
-    </div>
-    <div class="seccion">
-        <form method="post" action="#" id="frm_create_person" enctype="multipart/form-data">
-            <div class="mb-3">
-                <label for="PRSN_name" class="form-label">Nombre completo</label>
-                <div class="input-group">
-                    <input id="PRSN_name" type="text" name="PRSN_name" class="form-control" placeholder="Nombre" required>
-                    <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+<div class="create_person">
+    <form method="post" action="#" id="frm_create_person" enctype="multipart/form-data">
+        <div class="card">
+            <div class="card-header">
+                DATOS USUARIO
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="PRSN_name" class="form-label">Nombre completo</label>
+                            <div class="input-group">
+                                <input id="PRSN_name" type="text" name="PRSN_name" class="form-control" placeholder="Nombre" required>
+                                <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="PRSN_document" class="form-label">Número documento</label>
+                            <div class="input-group">
+                                <input id="PRSN_document" type="text" name="PRSN_document" class="form-control" placeholder="Documento" required>
+                                <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="PRSN_email" class="form-label">Correo electrónico</label>
+                            <div class="input-group">
+                                <input id="PRSN_email" type="email" name="PRSN_email" class="form-control" placeholder="Correo electrónico" required>
+                                <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="PRSN_phone" class="form-label">Teléfono</label>
+                            <div class="input-group">
+                                <input id="PRSN_phone" type="text" name="PRSN_phone" class="form-control" placeholder="Teléfono" required>
+                                <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>    
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                            <label for="PRSN_position" class="form-label">Cargo</label>
+                            <div class="input-group">
+                                <input id="PRSN_position" type="text" name="PRSN_position" class="form-control" placeholder="Cargo" required>
+                                <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        ALERT
+                    </div>
+                </div>    
+            </div>
+        </div>
+
+
+        <div class="card">
+            <div class="card-header">
+                DOCUMENTOS
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="DCPR_name_1" class="form-label">Acuerdo de confidencialidad</label>
+                            <input id="DCPR_name_1" type="file" name="DCPR_name_1" class="form-control" required>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="mb-3">
+                            <label for="DCPR_name_2" class="form-label">Documento</label>
+                            <input id="DCPR_name_2" type="file" name="DCPR_name_2" class="form-control" required>
+                        </div>
+                    </div>
                 </div>
             </div>
-                
-            <div class="mb-3">
-                <label for="PRSN_document" class="form-label">Numero Documento</label>
-                <div class="input-group">
-                    <input id="PRSN_document" type="text" name="PRSN_document" class="form-control" placeholder="Documento" required>
-                    <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
-                </div>
-            </div>
+        </div>
 
-            <div class="mb-3">
-                <label for="PRSN_email" class="form-label">Correo electronico</label>
-                <div class="input-group">
-                    <input id="PRSN_email" type="email" name="PRSN_email" class="form-control" placeholder="Correo electronico" required>
-                    <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
-                </div>
-            </div>
-
-            <div class="mb-3">
-                <label for="PRSN_phone" class="form-label">Telefono</label>
-                <div class="input-group">
-                    <input id="PRSN_phone" type="text" name="PRSN_phone" class="form-control" placeholder="Telefono" required>
-                    <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
-                </div>
-            </div>
-
-            <div class="mb-3">
-                <label for="PRSN_position" class="form-label">Cargo</label>
-                <div class="input-group">
-                    <input id="PRSN_position" type="text" name="PRSN_position" class="form-control" placeholder="Cargo" required>
-                    <button type="button" class="btn btn-outline-secondary copy-btn">Copy</button>
-                </div>
-            </div>
-            <div class="mb-3">
-                <label for="DCPR_name_1" class="form-label">Acuerdo de confidencialidad</label>
-                <input id="DCPR_name_1" type="file" name="DCPR_name_1" class="form-control" required>
-            </div>
-
-            <div class="mb-3">
-                <label for="DCPR_name_2" class="form-label">Documento</label>
-                <input id="DCPR_name_2" type="file" name="DCPR_name_2" class="form-control" required>
-            </div>
-
-            <div class="card-footer">
-                <button type="submit" class="btn btn-success btn-block">CREAR</button>
-                <button type="button" class="btn btn-danger" id="back" name="back">ATRAS</button>
-            </div>
-        </form>
-    </div>
+        <div class="area-buttons">
+            <button type="submit" class="btn btn-primary btn-block">Crear usuario</button>
+            <button type="button" class="btn btn-danger" id="back" name="back">Atras</button>
+        </div>
+    </form>
 </div>
+
+<style>
+.create_person{
+    max-width: 50%;
+    margin: 0 auto;
+}
+
+.form-control{
+    border-radius: 31px  0 0 31px!important;
+}
+
+.area-buttons{
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-evenly;
+}
+
+</style>
 
 <script type="text/javascript" src="<?= base_url('assets/scripts/persons/createPerson.js'); ?>"></script>
