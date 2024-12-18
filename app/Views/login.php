6ccfcd02ffb7e113281e3a49087859053cbe7f5f
Daniel Avendaño <dalaven1996@gmail.com>
2024-12-18 10:47:54 -0500

    Login updated

--- app/Views/login.php
+++ app/Views/login.php
@@ -10,7 +10,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </head>
     <body>
-        <div class="row login-page">
+        <div class="row login-page m-0">
             <div class="col-6 banner-section">
             <h1  class="text-center susi"><?= lang("General.appName") ?></h1>    
             <h3  class="text-center"> Sistema Unificado de Servicios de la Información </h3>
@@ -21,21 +21,21 @@
             <div class="col-6 form-section">
                 <div class="img-unidad"></div>
                 <h3 class="text-center title fw-bolder">Inicio sesión</h3>
-                <h5 class="text-center fw-lighter">Ingresa tus credenciales</h5>
+                <h5 class="text-center fw-lighter mb-5">Ingresa tus credenciales</h5>
                 <form method="post" action="<?= base_url('users/login'); ?>" id="frm_login">
                     <div class="mb-3">
                         <label class="form-label"><?= lang("General.user") ?></label>
                         <input id="USER_username" type="text" name="USER_username" class="form-control" required>
                     </div>
-                    <div class="mb-3">
+                    <div class="mb-3 mt-4">
                         <label class="form-label">Contraseña:</label>
                         <input id="USER_password" type="password" name="USER_password" class="form-control" required>
                     </div>
                     <div class="d-grid">
-                        <button type="submit" class="btn btn-primary btn-login">Ingresar</button>
+                        <button type="submit" class="btn btn-primary btn-login mt-4">Ingresar</button>
                     </div>
                 </form>
-                <p class="text-center">¿Olvidó su contraseña?</p>
+                <p class="text-center hidden">¿Olvidó su contraseña?</p>
             </div>
         </div>
         <div class="alerta hidden" id="alerta">
